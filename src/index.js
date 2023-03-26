02b0a0db9cf11b85b240def878e598257c201039
Varnit Agrawal <varnit.agrawal24@gmail.com>
2023-03-26 14:27:42 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+class API{
+    url;
+    method="GET";
+    #secure;
+    constructor(url){
+        this.url=url;
+    }
+    isSecure(){
+        if(this.url.includes("https")) return true;
+        return false;
+    }
+}
+
 
 
 module.exports = { API }
